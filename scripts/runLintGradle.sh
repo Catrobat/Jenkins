98101572981004a8efde67b1623146a0ff1ceebf
Thomas Kaufmann <kaufmaster@gmail.com>
2014-01-11 22:01:18 +0100

    add lint file for gradle
    gradle puts classfiles in other folder structure!
    classpath parameter needed for full lint checks

---
+++ scripts/runLintGradle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash      
+
+if [ "$#" -ne 2 ] 
+then
+  echo "Usage: ./runLint.sh androidSdkPath workspace"
+  exit 1
+fi
+
+androidSdkPath=$(echo $1 | sed 's/\/$//g')
+workspace=$(echo $2 | sed 's/\/$//g')
+
+echo "------ LINT CHECKER ------"
+
+$androidSdkPath/tools/lint --config $workspace/catroid/lint.xml --showall --xml $workspace/reports/catroid_lint_results.xml --classpath build/classes/debug/ catroid
+$androidSdkPath/tools/lint --config $workspace/catroidTest/lint.xml --showall --xml $workspace/reports/catroidTest_lint_results.xml --classpath build/classes/test/debug catroidTest
