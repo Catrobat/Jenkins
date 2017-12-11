d6f6e5d08ffecb67d8d129a41c9790919bb61d76
Michael Musenbrock <redeamer@gmx.net>
2017-12-11 16:39:53 +0100

    JENKINS-181: Fix acceptance of android licenses
    Update android-sdk-license hash to latest available

--- job_dsl/src/main/lib/AndroidJobBuilder.groovy
+++ job_dsl/src/main/lib/AndroidJobBuilder.groovy
@@ -89,7 +89,7 @@ fi
 if [ -d "$TOOLS_DIR" ]; then
     LICENSES_DIR="$TOOLS_DIR/android-sdk/licenses"
     mkdir -p "$LICENSES_DIR"
-    echo "\\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "${LICENSES_DIR}/android-sdk-license"
+    echo "\\nd56f5187479451eabf01fb78af6dfcb131a6481e" > "${LICENSES_DIR}/android-sdk-license"
     echo "\\n84831b9409646a918e30573bab4c9c91346d8abd" > "${LICENSES_DIR}/android-sdk-preview-license"
     exit 0
 else
