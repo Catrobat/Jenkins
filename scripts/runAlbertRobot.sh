945c64559e6cef23fc580d36c92e487fc8afb313
Michael Musenbrock <michael.musenbrock@gmail.com>
2018-06-20 13:50:12 +0200

    Remove unused scripts folder

--- scripts/runAlbertRobot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 1 ] 
-then
-  echo "Usage: ./runAlbertRobot.sh bluetoothAlbertServer"
-  exit 1
-fi
-
-java -jar $1/bluetoothDummyServerAlbertAndSharedVariables.jar
--- scripts/startSeleniumTests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make stop-selenium
-make refresh-all
-xvfb-run --server-args="-screen 0, 840x600x24" make run-selenium-tests
