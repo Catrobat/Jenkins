f878558c100e3509e59dfc7ffcfccc0e005097a6
Michael Musenbrock <michael.musenbrock@gmail.com>
2018-06-20 12:20:07 +0200

    JENKINS-208: Update emulator config to current values

--- job_dsl/src/main/lib/CatroidData.groovy
+++ job_dsl/src/main/lib/CatroidData.groovy
@@ -15,11 +15,12 @@ class CatroidData {
                              'thomasmauerhofer']
     def androidEmulatorParameters = [androidApi: '24', screenDensity: 'xhdpi', screenResolution: '768x1280', targetAbi: 'x86_64',
                                      noActivityTimeout: '1200',
-                                     hardwareProperties: ['hw.camera': 'yes', 'hw.ramSize': '800', 'hw.gpu.enabled': 'yes',
-                                                          'hw.camera.front': 'emulated',
-                                                          'hw.camera.back': 'emulated', 'hw.gps': 'yes',
+                                     hardwareProperties: ['hw.camera': 'yes', 'hw.ramSize': '2048', 'hw.gpu.enabled': 'yes',
+                                                          'hw.camera.front': 'emulated', 'hw.camera.back': 'emulated',
+                                                          'hw.gps': 'yes',
+                                                          'hw.mainKeys': 'no', 'hw.keyboard': 'yes',
                                                           'disk.dataPartition.size': '512M'],
-                                     commandLineOptions: '-no-boot-anim -noaudio -qemu -m 800 -enable-kvm']
+                                     commandLineOptions: '-no-boot-anim -noaudio']
     def debugApk = 'catroid/build/outputs/apk/catroid/debug/catroid-catroid-debug.apk'
     def staticAnalysisResults = [androidLint: 'catroid/build/reports/lint*.xml',
                                  checkstyle: 'catroid/build/reports/checkstyle.xml',
