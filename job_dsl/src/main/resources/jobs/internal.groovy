37b687aa4c8da48b783744392897ae0a21af977d
Andreas Finsterbusch <andreas.finsterbusch@student.tugraz.at>
2018-11-25 11:34:47 +0100

    JENKINS-234 - JobDSL: Port SeedJob to pipeline

--- job_dsl/src/main/resources/jobs/internal.groovy
+++ job_dsl/src/main/resources/jobs/internal.groovy
@@ -1,27 +1,14 @@
 // The file should not be named jenkins.groovy as that leads to a warning as there
 // is already a jenkins package.
-def internal = new JobsBuilder(this).folder('Jenkins', {})
+def internal = new JobsBuilder(this).folderAndView('Jenkins', {})
 
 internal.job("SeedJob") {
     htmlDescription(['Seed job to create all other jobs.'])
 
     jenkinsUsersPermissions()
-    label('master')
-    git(repo: 'https://github.com/Catrobat/Jenkins.git', branch: 'master')
-    continuous()
-    concurrentBuild(false)
-    nightly('H 23 * * *') // run the job before all other nightlies
-    shell('cp configs/log_parser_rules.groovy $JENKINS_HOME/')
-    steps {
-        jobDsl {
-            additionalClasspath('job_dsl/src/main/lib/')
-            targets('job_dsl/src/main/resources/jobs/*.groovy')
-            failOnMissingPlugin(true)
-            removedJobAction('DELETE')
-            removedViewAction('DELETE')
-            unstableOnDeprecation(true)
-        }
-    }
+    git(repo: 'https://github.com/Catrobat/Jenkins.git', branch: 'master', jenkinsfile: 'jenkinsfile.seedjob')
+    configure { it / definition / lightweight(true)}
 
     notifications()
 }
+
