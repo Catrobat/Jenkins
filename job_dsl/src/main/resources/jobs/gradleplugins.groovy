48f8cd951ea73f56e6b970be975b1ba9117c0806
Matthias Fuchs <matthias.s.fuchs@gmail.com>
2018-12-21 19:07:10 +0100

    JENKINS-255 Add multibranch job to build the gradle plugins.
    The LimitedEmulator label is used to support testing the emulator
    capabilities in the future.

---
+++ job_dsl/src/main/resources/jobs/gradleplugins.groovy
@@ -0,0 +1,12 @@
+new JobsBuilder(this).gitHubOrganization(null).job('GradlePlugins') {
+    htmlDescription(['Job is automatically started on a new commit or a new/updated pull request created on github.',
+                     'This job runs the Pipeline defined in the Jenkinsfile inside of the repository.',
+                     'The Pipeline builds the project and runs tests on it.'])
+
+    jenkinsUsersPermissions(Permission.JobRead, Permission.JobBuild, Permission.JobCancel, Permission.JobWorkspace)
+    anonymousUsersPermissions(Permission.JobRead) // allow anonymous users to see the results of PRs to fix their issues
+
+    gitHubOrganization()
+    jenkinsfilePath('Jenkinsfile')
+    labelForDockerBuild('LimitedEmulator')
+}
