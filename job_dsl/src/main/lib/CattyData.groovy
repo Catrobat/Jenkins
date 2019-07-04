0f86ed25a6307f0d7a302a06de8940c20dd43af1
Andreas Finsterbusch <andreas.finsterbusch@student.tugraz.at>
2019-07-04 12:42:49 +0200

    JENKINS-309 add Catty job to the seedjob

---
+++ job_dsl/src/main/lib/CattyData.groovy
@@ -0,0 +1,8 @@
+class CattyData {
+    def repoOwner = 'Catrobat'
+    def repoName = 'Catty'
+    def repo = 'https://github.com/Catrobat/Catty.git'
+    def branch = 'develop'
+    def githubUrl = 'https://github.com/Catrobat/Catty'
+    def githubOrganizationsJenkinsCredentialsRefId = 'github-organization-catrobatjenkins'
+}
