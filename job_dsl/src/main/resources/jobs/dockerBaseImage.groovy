ec77fcf14de6cd2d807b3a9e0201dc11067625d2
Johannes Trattner <johannes.trattner@student.tugraz.at>
2020-04-28 16:14:33 +0200

    JENKINS-375: added Create Jenkins Job via JobDSL to regurarly build docker base image

---
+++ job_dsl/src/main/resources/jobs/dockerBaseImage.groovy
@@ -0,0 +1,18 @@
+//Creates a pipeline in the jenkins base directory
+def baseImageJobBuilder = new JobsBuilder(this).pipeline()
+
+baseImageJobBuilder.job("Build_Docker_Base_Image") {
+    htmlDescription(['Builds the docker base Image and pushes it to the dockerhub'])
+
+    git(repo: 'https://github.com/Catrobat/Catroid', branch: '${gitBranch}', jenkinsfile: 'Jenkinsfile.baseDocker')
+
+    parameters {
+        gitParam('gitBranch') {
+            description('Select the branch you want to build e.g. origin/master.')
+            type('BRANCH')
+            defaultValue('origin/master')
+        }
+    }
+
+}
+
