7a4577725a4a38edfe09369d10c2fd126b566da2
Johannes Trattner <johannes.trattner@student.tugraz.at>
2020-07-30 14:25:13 +0200

    JENKINS-385: moved docker base image job parameters to job dsl script

--- job_dsl/src/main/resources/jobs/dockerBaseImage.groovy
+++ job_dsl/src/main/resources/jobs/dockerBaseImage.groovy
@@ -7,10 +7,13 @@ baseImageJobBuilder.job("Build_Docker_Base_Image") {
     git(repo: 'https://github.com/Catrobat/Catroid', branch: '${gitBranch}', jenkinsfile: 'Jenkinsfile.baseDocker')
 
     parameters {
+        booleanParam('TAG_STABLE', false, 'When selected image will be tagged stable')
+        booleanParam('TAG_TESTING', true, 'When selected image will be tagged testing')
+        stringParam('IMAGE_NAME', 'catrobat-android', 'Name for docker image to build')
         gitParam('gitBranch') {
             description('Select the branch you want to build e.g. origin/master.')
             type('BRANCH')
-            defaultValue('origin/develop')
+            defaultValue('origin/master')
         }
     }
     nightly()
